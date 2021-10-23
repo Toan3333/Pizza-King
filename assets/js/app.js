78f37b131aca337d44522ed4bea2ad34c6b048eb
Toan3333 <89564706+Toan3333@users.noreply.github.com>
2021-10-23 14:58:20 +0700

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,37 +7,76 @@ let btnDanger = document.querySelector("#btn__danger");
 let loginUser = document.querySelector("#login__user");
 let loginBox = document.querySelector(".login__box");
 let formClose = document.querySelector("#form-close");
+let headerShop = document.querySelector("#shop");
+let headerCategori = document.querySelector("#categories");
+let headernavShop = document.querySelector(".header-navigation-shop");
+let headernavShop__2 = document.querySelector(".header-navigation-shop-2");
+let headerProduct = document.querySelector("#product");
+let headernavShop__3 = document.querySelector(".header-navigation-shop-3");
+let barBtn = document.querySelector("#bar");
+let barTimes = document.querySelector("#times");
+let headerNav = document.querySelector(".header-nav-center");
 
+
+const accordion = document.getElementsByClassName("shop-1");
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+  });
+}
+
+// Xử lý đóng / mở modal
 checkClose.addEventListener("click", function () {
   modal.remove();
 });
 
+// Xử lý khi click menu nav
+headerShop.addEventListener("click", function () {
+  headernavShop.classList.toggle("active");
+});
+
+headerCategori.addEventListener("click", function () {
+  headernavShop__2.classList.toggle("active");
+});
+
+headerProduct.addEventListener("click", function () {
+  headernavShop__3.classList.toggle("active");
+});
+
+// Xử lý ẩn hiện giỏ hàng
 shoppingcartBtn.addEventListener("click", function () {
   shoppingCart.classList.toggle("active");
 });
 
+barBtn.addEventListener("click", function () {
+  headerNav.classList.add("active");
+});
+
+barTimes.addEventListener("click", function () {
+  headerNav.classList.remove("active");
+});
+
+// Xử lý hiện đăng nhập
 loginUser.addEventListener("click", function () {
   loginBox.classList.add("active");
 });
 
+// Xử lý đóng đăng nhập
 formClose.addEventListener("click", function () {
   loginBox.classList.remove("active");
 });
 
-// Xoa cart
-
-// for (i = 0; i < shoppingCart.length; i++) {
-//   btnDanger.addEventListener('click', function(xoa){
-//     shoppingcartBox.remove();
-//   });
-// }
-
-function addCart(x) {
-  var product__box = x.parentElement.children;
-  var img = product__box[0].children[0].src;
-  console.log(img);
+// xóa cart
+var remove_cart = document.getElementsByClassName("btn__danger");
+for (var i = 0; i < remove_cart.length; i++) {
+  var button = remove_cart[i]
+  button.addEventListener("click", function () {
+    var button_remove = event.target
+    button_remove.parentElement.remove()
+  })
 }
 
+// Active link
 function menu(element) {
   let tabs = document.getElementsByClassName("item");
   for (i = 0; i < tabs.length; i++) {
@@ -46,12 +85,18 @@ function menu(element) {
   element.style.color = "#fb4734";
 }
 
+// Slider
+
 var swiper = new Swiper(".hero-slider", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
@@ -128,6 +173,8 @@ var swiper = new Swiper(".brand-slider", {
     },
   },
 });
+
+// Xử lý scroll
 window.onscroll = () => {
   if (window.scrollY > 60) {
     document.querySelector("#scroll-top").classList.add("active");
@@ -136,6 +183,7 @@ window.onscroll = () => {
   }
 };
 
+// Load trang
 function loader() {
   document.querySelector(".loader-container").classList.add("fade-out");
 }
